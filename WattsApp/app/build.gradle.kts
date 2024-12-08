9c01670fb80d260d028d51166a7dd7fd17ceec2d
Mika Miettinen <mika.olavi.miettinen@gmail.com>
2024-12-09 00:01:44 +0200

    Page1 bar chart and data fetch with presentation added. Page3 button colors changed.

--- WattsApp/app/build.gradle.kts
+++ WattsApp/app/build.gradle.kts
@@ -51,6 +51,9 @@ android {
 
 dependencies {
 
+    implementation (libs.kotlinx.coroutines.android)
+    implementation (libs.retrofit)
+    implementation (libs.converter.gson)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
@@ -60,6 +63,7 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     implementation(libs.androidx.navigation.compose)
+    implementation(libs.androidx.navigation.runtime.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
